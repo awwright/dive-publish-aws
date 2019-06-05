574680ae87da303a087a0338b89009f70188214a
Austin Wright <aaa@bzfx.net>
2019-06-04 23:34:21 -0700

    Dependency update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
+"use strict";
 
 const mapPromise = require('bluebird').map;
 
 exports.run = uploadResources;
 async function uploadResources(app, s3, bucket, base, prefix, pretend){
+	if(!bucket) throw new Error('bucket argument required');
+
 	var resources = await app.listing();
 	// TODO: request the default 404 error document,
 	// Verify it's not a resource defined by this,
@@ -12,46 +15,45 @@ async function uploadResources(app, s3, bucket, base, prefix, pretend){
 	var websiteInformation = await s3.getBucketWebsite({Bucket: bucket}).promise();
 	console.log(websiteInformation);
 
-	function writeResource(uri){
+	function writeResource(rsc){
+		var uri = rsc.uri;
 		var path = uri.substring(base.length);
 		// But require that the filepath identifies a directory
 		if(path[0]!='/') return;
 		if(path[path.length-1]==='/') return;
 		var key = prefix + path.substring(1);
 		// console.log(`${uri} -> ${key}`);
-		return app.prepare(uri).then(function(rsc){
-			// If --pretend is specified, bail as late as possible (which is here)
-			if(pretend){
-				return;
+		// If --pretend is specified, bail as late as possible (which is here)
+		if(pretend){
+			return;
+		}
+		var req = {
+			headers: {
+				Accept: 'application/xhtml+xml',
 			}
-			var req = {
-				headers: {
-					Accept: 'application/xhtml+xml',
-				}
-			};
-			return rsc.renderString(req).then(function(res){
-				var ct = res.getHeader('Content-Type');
-				var statusCode = res.statusCode || 200;
-				if(statusCode===200 && ct.length){
-					return s3.putObject({
-						Bucket: bucket,
-						Key: key,
-						Body: res.body,
-						ContentType: ct,
-					}).promise().then(function(op){
-						console.log(statusCode, uri, op);
-						return `${statusCode} ${uri}`;
-					});
-				}else{
-					console.error('Status code '+statusCode);
-					throw new Error(statusCode+' <'+uri+'>');
-				}
-			});
+		};
+		return rsc.renderString(req).then(function(res){
+			var ct = res.getHeader('Content-Type');
+			var statusCode = res.statusCode || 200;
+			if(statusCode===200 && ct.length){
+				return s3.putObject({
+					Bucket: bucket,
+					Key: key,
+					Body: res.body,
+					ContentType: ct,
+				}).promise().then(function(op){
+					console.log(statusCode, uri, op);
+					return `${statusCode} ${uri}`;
+				});
+			}else{
+				console.error('Status code '+statusCode);
+				throw new Error(statusCode+' <'+uri+'>');
+			}
 		});
 	}
 
-	return await mapPromise(resources.filter(function(uri){
+	return await mapPromise(resources.filter(function(resource){
 		// Strip trailing slash, if any
-		return uri.substring(0, base.length)===base;
+		return resource.uri.substring(0, base.length)===base;
 	}), writeResource, {concurrency: 10});
 }
